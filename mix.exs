dfe4d5e9844621b74ecdcd953f0561d1c4497281
KingdomCoder <kingdomcoding@gmail.com>
2024-12-04 23:23:23 +0100

    debug: update ex_ice

--- mix.exs
+++ mix.exs
@@ -57,7 +57,7 @@ defmodule ExWebRTC.MixProject do
   defp deps do
     [
       {:ex_sdp, "~> 1.0"},
-      {:ex_ice, git: "https://github.com/kingdomcoding/ex_ice.git", ref: "7cabe84ef21cf3e30821f1b9dd593aa9eff2d033", override?: true},
+      {:ex_ice, git: "https://github.com/kingdomcoding/ex_ice.git", ref: "6fe2158afa8f06c619653a5ec0925bff54801d75", override?: true},
       {:ex_dtls, "~> 0.16.0"},
       {:ex_libsrtp, "~> 0.7.1"},
       {:ex_rtp, "~> 0.4.0"},
